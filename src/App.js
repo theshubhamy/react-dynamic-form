26c219ecdf300bfee0ef3a320ca5f226ec5c2107
theshubhamy <devshubhamyadav@gmail.com>
2023-04-16 06:01:08 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import { Box, TextField, Grid, Button, Typography } from "@mui/material";
+import { Terminal } from "xterm";
+import "xterm/css/xterm.css";
+const App = () => {
+  const terminalRef = useRef(null);
+  const terminal = new Terminal();
+  useEffect(() => {
+    terminal.open(terminalRef.current);
+  }, []);
 
-function App() {
+  const [formFields, setFormFields] = useState([
+    { name: "", email: "", phone: "" },
+  ]);
+
+  const handleFormChange = (event, index) => {
+    let data = [...formFields];
+    data[index][event.target.name] = event.target.value;
+    setFormFields(data);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    terminal.write(formFields);
+  };
+
+  const addFields = () => {
+    let object = {
+      name: "",
+      email: "",
+      phone: "",
+    };
+
+    setFormFields([...formFields, object]);
+  };
+
+  const removeFields = (index) => {
+    let data = [...formFields];
+    data.splice(index, 1);
+    setFormFields(data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Typography variant="h3" textAlign={"center"}>
+        Dynamic Form
+      </Typography>
+      <Box component="form" onSubmit={handleSubmit}>
+        {formFields.map((form, index) => {
+          return (
+            <Grid container spacing={2} key={index}>
+              <Grid item xs={12} sm={3}>
+                <TextField
+                  name="name"
+                  label="Name"
+                  size="small"
+                  onChange={(event) => handleFormChange(event, index)}
+                  value={form.name}
+                />
+              </Grid>
+
+              <Grid item xs={12} sm={3}>
+                <TextField
+                  name="email"
+                  label="email"
+                  size="small"
+                  onChange={(event) => handleFormChange(event, index)}
+                  value={form.email}
+                />
+              </Grid>
+              <Grid item xs={12} sm={3}>
+                <TextField
+                  name="phone"
+                  size="small"
+                  label="Phone"
+                  onChange={(event) => handleFormChange(event, index)}
+                  value={form.phone}
+                />
+              </Grid>
+              <Grid item xs={12} sm={3}>
+                <Button
+                  sx={{ width: "100%" }}
+                  onClick={() => removeFields(index)}
+                  variant="contained"
+                >
+                  Remove
+                </Button>
+              </Grid>
+            </Grid>
+          );
+        })}
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "end",
+            alignItems: "center",
+            marginTop: 3,
+            gap: 1,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Button variant="contained" onClick={addFields}>
+            Add More
+          </Button>
+          <Button variant="contained" type="submit">
+            Submit
+          </Button>
+        </Box>
+      </Box>
+      <Box sx={{ height: "200px" }}>
+        <div ref={terminalRef} />
+      </Box>
+    </Box>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+const theme = createTheme();
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+    ,
   </React.StrictMode>
 );
 
